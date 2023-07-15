d8bb09e2b3bc26dc84102133e550aee96b519a3c
Abdelmathin Habachi <ahabachi@e3r10p12.1337.ma>
2023-07-15 15:05:46 +0100

    Updated

---
+++ assets/js/khouribga.js
@@ -0,0 +1,19 @@
+const _13teams = 
+[
+  {
+    KEY_RANK     : 1,
+    KEY_NAME     : 'abdelmathin_habachi',
+    KEY_LOGIN    : 'bhabachi',
+    KEY_IMAGE    : 'b7955ba06b187afcf037f75f677722dc/ahabachi.jpg',
+    KEY_LEVEL    : 9999.05,
+    KEY_TIME     : 0 
+  },
+  {
+    KEY_RANK     : 1,
+    KEY_NAME     : 'abdelmathin_habachi',
+    KEY_LOGIN    : 'ahabachi',
+    KEY_IMAGE    : 'b7955ba06b187afcf037f75f677722dc/ahabachi.jpg',
+    KEY_LEVEL    : 9.08,
+    KEY_TIME     : 0 
+  }
+];
